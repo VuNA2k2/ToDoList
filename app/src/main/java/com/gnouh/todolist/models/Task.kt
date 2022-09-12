68bcb4032e6e04a438a1cc502a1dbacf229148c0
VuNAP <vuvovan2k2>
2022-09-12 16:15:37 +0700

    [UPDATE]

--- app/src/main/java/com/gnouh/todolist/models/Task.kt
+++ app/src/main/java/com/gnouh/todolist/models/Task.kt
@@ -1,20 +1,29 @@
 package com.gnouh.todolist.models
 
 import android.annotation.SuppressLint
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gnouh.todolist.constants.*
 import java.io.Serializable
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.util.*
 
-class Task(var title: String, var description: String = "", var deadline: String) : Serializable {
-    var timeCreate: String
+@Entity(tableName = TASK_TABLE_NAME)
+class Task(
+    @ColumnInfo(name = TASK_NAME_COL) var title: String,
+    @ColumnInfo(name = TASK_DESCRIPTION_COL) var description: String = "",
+    @ColumnInfo(name = TASK_DEADLINE_COL) var deadline: String
+) : Serializable {
+    @ColumnInfo(name = TASK_TIME_CREATE_COL) var timeCreate: String = simpleDateFormat.format(Calendar.getInstance().time)
+    @PrimaryKey(autoGenerate = true)
+    var id = 0
     companion object {
         private const val pattern = "dd/MM/yyyy HH:mm"
+
         @SuppressLint("SimpleDateFormat")
         val simpleDateFormat = SimpleDateFormat(pattern)
     }
-    init {
-        timeCreate = simpleDateFormat.format(Calendar.getInstance().time)
-        val date = LocalDate.parse(timeCreate)
-    }
+
 }
